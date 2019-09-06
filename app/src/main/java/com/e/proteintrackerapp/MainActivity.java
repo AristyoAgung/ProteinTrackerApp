b53812a41ece32cefa3eb5965b8473fa3c501c16
AristyoAgung <aristyo.agung@gmail.com>
2019-09-06 18:38:27 +0700

    Finished?

--- app/src/main/java/com/e/proteintrackerapp/MainActivity.java
+++ app/src/main/java/com/e/proteintrackerapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.e.proteintrackerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View.OnClickListener tableButtonListener = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,TableLayoutActivity.class);
+                startActivity(intent);
+            }
+        };
+        Button tableBtn = (Button)findViewById(R.id.buttonTable);
+        tableBtn.setOnClickListener(tableButtonListener);
+        View.OnClickListener proteinButtonListener = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ProteinTrackerActivity.class);
+                startActivity(intent);
+            }
+        };
+        Button proteinBtn = (Button)findViewById(R.id.buttonProtein);
+        proteinBtn.setOnClickListener(proteinButtonListener);
     }
 }
---
+++ app/src/main/java/com/e/proteintrackerapp/ProteinTrackerActivity.java
@@ -0,0 +1,14 @@
+package com.e.proteintrackerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProteinTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_protein);
+    }
+}
